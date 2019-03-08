06e6ce8b0344c84955d1ae4632a293d45395ca08
admin <admin@DESKTOP-PGDKJ9D>
2019-03-08 12:20:21 +0800

    initial

---
+++ src/main/java/LiuWang/SootTest/App.java
@@ -0,0 +1,13 @@
+package LiuWang.SootTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" ); 
+    }
+}
