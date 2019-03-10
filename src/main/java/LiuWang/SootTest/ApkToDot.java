4e379037495f81aa4fd804a7243d768b9d781a2f
admin <admin@DESKTOP-PGDKJ9D>
2019-03-10 16:38:50 +0800

    添加命令行处理，能够实现单文件转换或者批量转化

--- src/main/java/LiuWang/SootTest/ApkToDot.java
+++ src/main/java/LiuWang/SootTest/ApkToDot.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ApkToDot {
+	
 	/**
-	 * 
 	 * transform one apk to .dot file
 	 * @param inputDir:the directory stores the input .apk files
 	 * @param outputDir:the directory to store output .dot files
@@ -14,33 +14,46 @@ public class ApkToDot {
 	 * @param jarsPath : "android-SDK\\platforms\\"
 	 * @param androidCallbackPath: an empty .txt file or others
 	 */
-	public static void run(String inputDir, String outputDir, String apkName, String jarsPath, String androidCallbackPath){
-		System.out.println( "-------- transforming " + apkName +"\t-----------");
-        AndroidFlowDroidGraph afdg = new AndroidFlowDroidGraph(inputDir+"\\"+apkName, jarsPath, androidCallbackPath);
-//        System.out.println(afdg.getCallGraph());
-//        System.out.println(afdg.getMainMethod());
+	public static void runSingle(String apkPath, String outputDir, String jarsPath, String androidCallbackPath){
+		File apkFile = new File(apkPath);
+		System.out.println( "-------- transforming " + apkFile.getName() +"\t-----------");
+        AndroidFlowDroidGraph afdg = new AndroidFlowDroidGraph(apkPath, jarsPath, androidCallbackPath);
         DotGraphUtil.CGToDotWithMethodsName(afdg.getCallGraph(), afdg.getMainMethod());
-        DotGraphUtil.exportDotGraph(apkName, outputDir, DotGraphUtil.dotGraph);
+        DotGraphUtil.exportDotGraph(apkFile.getName(), outputDir, DotGraphUtil.dotGraph);
 	}
 	
+	
+	/**
+	 * transform a directory of apk files to dot files
+	 * @param inputDir:the dir storing apk files being transformed
+	 * @param outputDir:the dir storing dot files
+	 * @param jarsPath:android sdk platforms path
+	 * @param androidCallbackPath: android callback text file
+	 */
 	public static void runBatch(String inputDir, String outputDir, String jarsPath, String androidCallbackPath){
-		List<String> apkFilesNameList = getApkFiles(inputDir);
-		for(String name : apkFilesNameList){
-			run(inputDir, outputDir, name, jarsPath, androidCallbackPath);
+		List<String> apkAbsPathList = getApkFiles(inputDir);
+		for(String apkAbsPath : apkAbsPathList){
+			runSingle(apkAbsPath, outputDir, jarsPath, androidCallbackPath);
 		}
 	}
 	
+	
+	
+	/**
+	 * get absolute path of apk files in basedir
+	 * @param baseDir:the dir storing apk files
+	 * @return a list of apk abspath
+	 */
 	private static List<String> getApkFiles(String baseDir){
 		List<String> result = new ArrayList<String>();
 		File dir = new File(baseDir);
 		System.out.println(dir.getAbsolutePath()+", isfile:"+dir.isFile());
 		File[] dirFiles = dir.listFiles();
 		for(File temp : dirFiles){
-			//查找指定的文件
+			//查找.apk文件
 			if(temp.isFile() && temp.getAbsolutePath().endsWith(".apk") ){
-				System.out.println(temp.getName() + " " + temp.getAbsolutePath());
-//				readFileContent(temp);
-				result.add(temp.getName());
+				System.out.println(temp.getAbsolutePath());
+				result.add(temp.getAbsolutePath());
 			}
 		}
 		return result;
--- src/main/java/LiuWang/SootTest/App.java
+++ src/main/java/LiuWang/SootTest/App.java
@@ -1,23 +1,79 @@
 package LiuWang.SootTest;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
- * Hello world!
- *
+ * 
+ * @author liuwang 
+ * @department WHU CS
+ * @date 2019/3/9
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "==========   Begin!  =============" ); 
-//        ApkToDot.run(
-//        		"testAPKs", 
-//        		"testOutput", 
-//        		"enriched1.apk", 
-//        		"E:\\android-sdk-windows\\platforms", 
-//        		"AndroidCallbacks.txt");
-        
-        ApkToDot.runBatch("testAPKs", 
-        		"testOutput",  
-        		"E:\\android-sdk-windows\\platforms", 
-        		"AndroidCallbacks.txt");
-        System.out.println("==========   Finished!  =============");
+    	CommandLineParser parser = new BasicParser( );
+		Options options = new Options( );
+		options.addOption("h", "helpMsg", false, "Print this usage information.");
+		options.addOption("f", "typeFile", false, "transfor an apk file to dot file." );
+		options.addOption("d", "typeDir", false, "transfor a directory of apk files to dot files." );
+		options.addOption("i", "input", true, "the path of input directory storing .apk files or of an apk file.");
+		options.addOption("o", "outputDir", true, "the output directory to store output .dot files.");
+		options.addOption("s", "sdkDir", true, "the android sdk directory.");
+		options.addOption("c", "callbackFile", true, "the call backs file.");
+		// Parse the program arguments and do transforming
+		CommandLine cl;
+		try {
+			cl = parser.parse( options, args );
+			
+			System.out.println(cl.hasOption('h')+""+cl.hasOption('i')+cl.hasOption('o')+cl.hasOption('s')+cl.hasOption('c'));
+
+			if( cl.hasOption('h') ) {
+				System.out.println(helpMsg);
+				System.exit(0);
+			}
+			
+			if( cl.hasOption('f') && cl.hasOption('d') || !cl.hasOption('f') && !cl.hasOption('d')) {
+				throw new IllegalArgumentException("Ether -f or -d!");
+			} else {
+				if( !cl.hasOption('i') || !cl.hasOption('o') || !cl.hasOption('s') || !cl.hasOption('c') ){
+					throw new IllegalArgumentException("The options(-i, -o, -s and -c) are nessesary!");
+				}
+				String input = cl.getOptionValue('i'); 			// "testAPKs";
+				String outputDir = cl.getOptionValue('o'); 		// "testOutput";
+				String sdkDir = cl.getOptionValue('s'); 		// "E:\\android-sdk-windows\\platforms";
+				String callbackFile = cl.getOptionValue('c'); 	// "AndroidCallbacks.txt";
+				System.out.println("=================== Begin transforming ===================");
+				if( cl.hasOption('f') ) {
+					ApkToDot.runSingle(input, outputDir, sdkDir, callbackFile);
+				} else {
+					ApkToDot.runBatch(input, outputDir, sdkDir, callbackFile);
+				}
+				System.out.println("===================      Finished!     ===================");
+			}	
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Parse Error\n" + helpMsg);
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		} catch (Exception e){
+			System.out.println(e);
+		}
+		
     }
+    
+    private static final String helpMsg = 
+    		"apk to dot 0.0.1\tHelp messages:\n"
+    		+ "-f\ttransfor an apk file to dot file.\n"
+    		+ "-d\ttransfor a directory of apk files to dot files.\n"
+    		+ "-i\tthe input directory storing .apk files.\n"
+    		+ "-o\tthe output directory to store .dot files.\n"
+    		+ "-s\tthe android sdk directory, like \"E:\\android-sdk-windows\\platforms\".\n"
+    		+ "-c\tthe call backs file. It can be an empty file but nessesary.\n"
+    		+ "Example 1:java -jar apk2dot.jar -f -i chrome.apk -o outdir -s E:\\android-sdk-windows\\platforms -c cb.txt\n"
+    		+ "Example 2:java -jar apk2dot.jar -d -i inputdir -o outdir -s E:\\android-sdk-windows\\platforms -c cb.txt\n";
 }
